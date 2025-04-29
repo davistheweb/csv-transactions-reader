f8a299a8182392a2cc5708b334921407c57bc1ea
davistheweb <josiahdave001@gmail.com>
2025-04-29 19:31:41 +0100

    :

--- views/transactions.php
+++ views/transactions.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <title>Transaction Table</title>
-    <link href="bootstrap.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   </head>
   <body class="bg-light p-5">
     <div class="container">
