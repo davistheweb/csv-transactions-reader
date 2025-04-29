2f142161a0ddcbf2f002cf24011d27db94722071
davistheweb <josiahdave001@gmail.com>
2025-04-29 19:26:52 +0100

    first commit

---
+++ app/App.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+function getTransactionFiles(string $dirPath): array{
+    $files = [];
+   
+    foreach(scandir($dirPath) as $file){
+
+         if(is_dir($file)) continue;
+         $files[] = $dirPath .$file;
+        //var_dump($file);
+    }
+
+    return $files;
+}
+
+function getTransactions(string $fileName): array {
+    if(!file_exists($fileName)){
+        trigger_error('File "'. $fileName . '"does not exists.', E_USER_ERROR);
+    }
+
+    $file = fopen($fileName, 'r');
+
+    fgetcsv($file);
+
+    $transactions = [];
+
+    while(($transaction = fgetcsv($file))!== false){
+        $transactions[] = $transaction;
+    }
+
+    return $transactions;
+}
---
+++ views/transactions.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Transaction Table</title>
+    <link href="bootstrap.css" rel="stylesheet" />
+  </head>
+  <body class="bg-light p-5">
+    <div class="container">
+      <h2 class="mb-4">Transaction History</h2>
+      <table class="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Check #</th>
+            <th>Description</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>01/01/2025</td>
+            <td>3478</td>
+            <td>Transaction 1</td>
+            <td class="text-success">-$456.39</td>
+          </tr>
+          <tr>
+            <td>01/02/2025</td>
+            <td></td>
+            <td>Transaction 2</td>
+            <td class="text-primary">$945.62</td>
+          </tr>
+          <tr>
+            <td>01/03/2025</td>
+            <td>1158</td>
+            <td>Transaction 3</td>
+            <td class="text-success">-$642.17</td>
+          </tr>
+          <tr>
+            <td>01/04/2025</td>
+            <td>7093</td>
+            <td>Transaction 4</td>
+            <td class="text-primary">$1287.44</td>
+          </tr>
+          <tr>
+            <td>01/05/2025</td>
+            <td></td>
+            <td>Transaction 5</td>
+            <td class="text-primary">$355.61</td>
+          </tr>
+          <tr>
+            <td>01/06/2025</td>
+            <td>8819</td>
+            <td>Transaction 6</td>
+            <td class="text-success">-$939.03</td>
+          </tr>
+          <tr>
+            <td>01/07/2025</td>
+            <td>7482</td>
+            <td>Transaction 7</td>
+            <td class="text-primary">$842.72</td>
+          </tr>
+          <tr>
+            <td>01/08/2025</td>
+            <td>2170</td>
+            <td>Transaction 8</td>
+            <td class="text-primary">$567.88</td>
+          </tr>
+          <tr>
+            <td>01/09/2025</td>
+            <td>9643</td>
+            <td>Transaction 9</td>
+            <td class="text-success">-$138.76</td>
+          </tr>
+          <tr>
+            <td>01/10/2025</td>
+            <td>2795</td>
+            <td>Transaction 10</td>
+            <td class="text-success">-$1003.25</td>
+          </tr>
+          <tr>
+            <td>01/11/2025</td>
+            <td></td>
+            <td>Transaction 11</td>
+            <td class="text-primary">$420.20</td>
+          </tr>
+          <tr>
+            <td>01/12/2025</td>
+            <td>3384</td>
+            <td>Transaction 12</td>
+            <td class="text-primary">$1279.55</td>
+          </tr>
+          <tr>
+            <td>01/13/2025</td>
+            <td>4941</td>
+            <td>Transaction 13</td>
+            <td class="text-success">-$211.14</td>
+          </tr>
+          <tr>
+            <td>01/14/2025</td>
+            <td></td>
+            <td>Transaction 14</td>
+            <td class="text-primary">$712.04</td>
+          </tr>
+          <tr>
+            <td>01/15/2025</td>
+            <td>6809</td>
+            <td>Transaction 15</td>
+            <td class="text-success">-$382.60</td>
+          </tr>
+          <tr>
+            <td>01/16/2025</td>
+            <td></td>
+            <td>Transaction 16</td>
+            <td class="text-primary">$931.70</td>
+          </tr>
+          <tr>
+            <td>01/17/2025</td>
+            <td>1294</td>
+            <td>Transaction 17</td>
+            <td class="text-primary">$168.38</td>
+          </tr>
+          <tr>
+            <td>01/18/2025</td>
+            <td></td>
+            <td>Transaction 18</td>
+            <td class="text-success">-$650.19</td>
+          </tr>
+          <tr>
+            <td>01/19/2025</td>
+            <td>2200</td>
+            <td>Transaction 19</td>
+            <td class="text-success">-$1123.45</td>
+          </tr>
+          <tr>
+            <td>01/20/2025</td>
+            <td>6641</td>
+            <td>Transaction 20</td>
+            <td class="text-primary">$946.80</td>
+          </tr>
+          <tr>
+            <td>01/21/2025</td>
+            <td>9158</td>
+            <td>Transaction 21</td>
+            <td class="text-primary">$777.99</td>
+          </tr>
+          <tr>
+            <td>01/22/2025</td>
+            <td></td>
+            <td>Transaction 22</td>
+            <td class="text-success">-$82.17</td>
+          </tr>
+          <tr>
+            <td>01/23/2025</td>
+            <td>3086</td>
+            <td>Transaction 23</td>
+            <td class="text-primary">$1301.00</td>
+          </tr>
+          <tr>
+            <td>01/24/2025</td>
+            <td>5012</td>
+            <td>Transaction 24</td>
+            <td class="text-success">-$296.45</td>
+          </tr>
+          <tr>
+            <td>01/25/2025</td>
+            <td>1176</td>
+            <td>Transaction 25</td>
+            <td class="text-success">-$172.92</td>
+          </tr>
+          <tr>
+            <td>01/26/2025</td>
+            <td>2883</td>
+            <td>Transaction 26</td>
+            <td class="text-primary">$841.77</td>
+          </tr>
+          <tr>
+            <td>01/27/2025</td>
+            <td>7731</td>
+            <td>Transaction 27</td>
+            <td class="text-primary">$1189.65</td>
+          </tr>
+          <tr>
+            <td>01/28/2025</td>
+            <td></td>
+            <td>Transaction 28</td>
+            <td class="text-success">-$104.38</td>
+          </tr>
+          <tr>
+            <td>01/29/2025</td>
+            <td>6022</td>
+            <td>Transaction 29</td>
+            <td class="text-success">-$219.84</td>
+          </tr>
+          <tr>
+            <td>01/30/2025</td>
+            <td></td>
+            <td>Transaction 30</td>
+            <td class="text-primary">$920.11</td>
+          </tr>
+          <tr>
+            <td>02/01/2025</td>
+            <td>7344</td>
+            <td>Transaction 31</td>
+            <td class="text-primary">$245.69</td>
+          </tr>
+          <tr>
+            <td>02/02/2025</td>
+            <td></td>
+            <td>Transaction 32</td>
+            <td class="text-primary">$739.98</td>
+          </tr>
+          <tr>
+            <td>02/03/2025</td>
+            <td>3829</td>
+            <td>Transaction 33</td>
+            <td class="text-success">-$379.44</td>
+          </tr>
+          <tr>
+            <td>02/04/2025</td>
+            <td>2560</td>
+            <td>Transaction 34</td>
+            <td class="text-primary">$682.02</td>
+          </tr>
+          <tr>
+            <td>02/05/2025</td>
+            <td>9304</td>
+            <td>Transaction 35</td>
+            <td class="text-primary">$1357.77</td>
+          </tr>
+          <tr>
+            <td>02/06/2025</td>
+            <td>1411</td>
+            <td>Transaction 36</td>
+            <td class="text-success">-$823.10</td>
+          </tr>
+          <tr>
+            <td>02/07/2025</td>
+            <td>4899</td>
+            <td>Transaction 37</td>
+            <td class="text-success">-$112.69</td>
+          </tr>
+          <tr>
+            <td>02/08/2025</td>
+            <td></td>
+            <td>Transaction 38</td>
+            <td class="text-primary">$1105.14</td>
+          </tr>
+          <tr>
+            <td>02/09/2025</td>
+            <td>6688</td>
+            <td>Transaction 39</td>
+            <td class="text-success">-$558.32</td>
+          </tr>
+          <tr>
+            <td>02/10/2025</td>
+            <td></td>
+            <td>Transaction 40</td>
+            <td class="text-primary">$780.94</td>
+          </tr>
+          <tr>
+            <td>02/11/2025</td>
+            <td>5000</td>
+            <td>Transaction 41</td>
+            <td class="text-primary">$359.22</td>
+          </tr>
+          <tr>
+            <td>02/12/2025</td>
+            <td>2486</td>
+            <td>Transaction 42</td>
+            <td class="text-success">-$1054.37</td>
+          </tr>
+          <tr>
+            <td>02/13/2025</td>
+            <td>1713</td>
+            <td>Transaction 43</td>
+            <td class="text-success">-$689.00</td>
+          </tr>
+          <tr>
+            <td>02/14/2025</td>
+            <td></td>
+            <td>Transaction 44</td>
+            <td class="text-primary">$986.54</td>
+          </tr>
+          <tr>
+            <td>02/15/2025</td>
+            <td>3059</td>
+            <td>Transaction 45</td>
+            <td class="text-success">-$343.89</td>
+          </tr>
+          <tr>
+            <td>02/16/2025</td>
+            <td>7780</td>
+            <td>Transaction 46</td>
+            <td class="text-primary">$1223.38</td>
+          </tr>
+          <tr>
+            <td>02/17/2025</td>
+            <td></td>
+            <td>Transaction 47</td>
+            <td class="text-success">-$92.33</td>
+          </tr>
+          <tr>
+            <td>02/18/2025</td>
+            <td></td>
+            <td>Transaction 48</td>
+            <td class="text-primary">$499.26</td>
+          </tr>
+          <tr>
+            <td>02/19/2025</td>
+            <td>8004</td>
+            <td>Transaction 49</td>
+            <td class="text-primary">$654.18</td>
+          </tr>
+          <tr>
+            <td>02/20/2025</td>
+            <td>4206</td>
+            <td>Transaction 50</td>
+            <td class="text-success">-$741.56</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
+</html>
